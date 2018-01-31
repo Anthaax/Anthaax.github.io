f1e8c4289d8524c2e6f8793186f503893c15ea17
Anthaxx <guillaume.fimes@laposte.net>
2018-01-31 22:32:16 +0100

    Almost finish important thing

--- js/CompChart.js
+++ js/CompChart.js
@@ -2,18 +2,21 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ["C#", "JavaScript", "SQL"],
+        labels: ["C#", "JavaScript", "SQL", "Autres"],
         datasets: [{
-            data: [12, 12, 12],
+            data: [15, 7, 8, 2],
             backgroundColor: [
                 'rgba(44, 62, 80, 0.2)',
                 'rgba(17, 131, 109, 0.2)',
                 'rgba(31, 118, 176, 0.2)',
+                'rgba(243, 156, 18, 0.2)'
             ],
             borderColor: [
                 'rgba(44, 62, 80, 1)',
                 'rgba(17, 131, 109, 1)',
                 'rgba(31, 118, 176, 1)',
+                'rgba(243, 156, 18, 1)'
+                
             ],
             borderWidth: 1
         }]
